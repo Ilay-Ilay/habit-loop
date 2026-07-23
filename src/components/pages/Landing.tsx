7b346f953f4693197a5ad09a8d84e267036f4d1d
Ilay Zeitlin <zeitlinilya@gmail.com>
2026-07-23 22:03:57 +0300

    hero link fixed

--- src/components/pages/Landing.tsx
+++ src/components/pages/Landing.tsx
@@ -28,7 +28,7 @@ function Landing() {
         <img
           src="/Hero.svg"
           alt="Hero"
-          className="absolute top-1/2 sm:top-10 w-[130%] max-w-none sm:w-360 z-0"
+          className="absolute top-1/2 sm:top-10 w-[130%] max-w-none sm:w-360 z-0 pointer-events-none"
         />
       </div>
     </section>
